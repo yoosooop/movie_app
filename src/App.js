426ae84e87ea2c5165631390dcbbb61dcff09d54
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-08-27 06:12:16 +0900

    firt study react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Potato from "./Potato";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <
+    div >
+    <
+    h1 > Hello < /h1>  <
+    Potato / >
+    <
+    /div>
   );
 }
 
 export default App;
+
+//component는 HTML을 반환하는 함수이다.
+//react는 component를 사용해서 HTML처럼 작성하려는 경우에 필요하다.
+//jsx : javascript 와 HTML 사이의 조합
