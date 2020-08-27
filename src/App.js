321f07604b98c3393d757c14add556f498019d9f
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-08-27 14:31:49 +0900

    2nd study

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food() {
+  return <h1 > I like potato < /h1>
+};
 
 function App() {
-  return ( <
-    div >
+  return ( < div >
     <
-    h1 > Hello < /h1>  <
-    Potato / >
+    h1 > Hello < /h1>  <Food / >
     <
     /div>
   );
-}
+};
 
 export default App;
 
 //component는 HTML을 반환하는 함수이다.
 //react는 component를 사용해서 HTML처럼 작성하려는 경우에 필요하다.
 //jsx : javascript 와 HTML 사이의 조합
+//component는 재사용 가능하다.
