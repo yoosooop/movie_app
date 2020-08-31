2dc6747b4a898b28871aad225526caec48728a4d
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-08-31 16:47:01 +0900

    Making different function

--- src/App.js
+++ src/App.js
@@ -25,12 +25,15 @@ const foodILike = [
   }
 ];
 
+function renderFood(dish){
+  console.log(dish)
+  return <Food name = {dish.name} picture={dish.image}/>
+}
+
 function App() {
   return ( 
   < div >
-  {foodILike.map(dish => (
-    <Food name = {dish.name} picture = {dish.image}/>
-  ))}
+  {foodILike.map(renderFood)}
   </div>
   );
 };
