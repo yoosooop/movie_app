265b41418317818ff4627791f65b83ab52884557
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-09-01 22:25:19 +0900

    Planning the Movie Component

--- README.md
+++ README.md
@@ -50,3 +50,14 @@ React.Component 에서 사용하는 유일한 function은 render function이다.
    
 ### Unmounting
 "component가 죽는 것"(페이지 바뀔 때)
+
+
+//component는 HTML을 반환하는 함수이다.
+//react는 component를 사용해서 HTML처럼 작성하려는 경우에 필요하다.
+//jsx : javascript 와 HTML 사이의 조합
+//component는 재사용 가능하다.
+
+//react magic에서 react는 우리가 전달한 props를 가져가는 일을 한다.
+//부모 컴포넌트에서 자식 컴포넌트로 원하는 많은 props를 보낼 수 있다.
+//컴포넌트는 대문자로 시작
+//map은 array의 각 item에서 function을 실행하는 array를 가지는 js 함수이며 그 함수의 result를 갖는 array를 나에게 준다.
--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
 
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movie : []
+  }
+  render(){
+    const {isLoading} = this.state
+    return<div>{isLoading ? "Loading..." : "We are Ready"}</div>;
+  };
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading : false, book : true})
+    },6000);
+  }
+}
+
+export default App;
+
+/*
 class App extends React.Component{
   state = {
     count : 0
@@ -25,15 +43,7 @@ class App extends React.Component{
   }
 }
 
+
 export default App;
 
-//component는 HTML을 반환하는 함수이다.
-//react는 component를 사용해서 HTML처럼 작성하려는 경우에 필요하다.
-//jsx : javascript 와 HTML 사이의 조합
-//component는 재사용 가능하다.
-
-//react magic에서 react는 우리가 전달한 props를 가져가는 일을 한다.
-//부모 컴포넌트에서 자식 컴포넌트로 원하는 많은 props를 보낼 수 있다.
-//컴포넌트는 대문자로 시작
-//map은 array의 각 item에서 function을 실행하는 array를 가지는 js 함수이며 그 함수의 result를 갖는 array를 나에게 준다.
-
+*/
