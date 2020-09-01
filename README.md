d346dabec59173beb2320a7eca01bdc2951e8729
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-09-01 14:46:01 +0900

    Class Components and State

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Movie app
 
 React JS Fundamentals Course(2020)
+
+## Class component
+- class 컴포넌트는 return을 하지 않는다. 이것은 function이 아니기 때문이다.
+- function 컴포넌트는 함수이며 무언가를 return 한다. 그리고 화면에 나타남
+- class 컴포넌트는 클래스이며 react component로 부터 확장되고 화면에 나타난다. 이것을 render method 안에 넣어야만 한다. 
+- 리액트는 **자동적으로** 모든 class component의 render method를 실행하고자 한다.
+
+
+## State
+- state는 object이고 component의 data를 넣을 공간이 있고 **이 데이터는 변한다**.
--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({name,picture,rating}) {
-//console.log(props.fav)
-  return (
-  <div>
-    <h2>I like {name}</h2>
-    <h4>{rating} / 5.0</h4>
-    <img src = {picture} alt = {name} />
-  </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-const foodILike = [
-  {
-    id : 1,
-    name : "김치",
-    image : "https://www.usc.go.kr/resources/templates/garlicmuseum/assets/images/sub/kimchi_img1.png",
-    rating : 4.5
-  },
-  {
-    id : 2,
-    name : "연어",
-    image : "https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*",
-    rating : 3.2
-  },
-  {
-    id : 3,
-    name : "김밥",
-    image : "https://homecuisine.co.kr/files/attach/images/140/001/083/558d170258752df2dd76bef00861497f.JPG",
-    rating : 2.1
+class App extends React.Component{
+  state = {
+    count : 0
   }
-];
-
-function App() {
-  return ( 
-  < div >
-  {foodILike.map(dish => (
-    <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>
-  ))}
-  </div>
-  );
-};
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  }
+  render() {
+     //즉시 호출되는 것이 아니라 click 했을 때만 function이 호출되길 원한다.
+    return <div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>
+  }
+}
 
 export default App;
 
@@ -60,3 +32,4 @@ export default App;
 //부모 컴포넌트에서 자식 컴포넌트로 원하는 많은 props를 보낼 수 있다.
 //컴포넌트는 대문자로 시작
 //map은 array의 각 item에서 function을 실행하는 array를 가지는 js 함수이며 그 함수의 result를 갖는 array를 나에게 준다.
+
