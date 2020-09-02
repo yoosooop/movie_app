13cdd65250c430be7cf6408d3997131297475d7c
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-09-03 01:35:42 +0900

    Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 
 class App extends React.Component{
@@ -17,11 +18,23 @@ class App extends React.Component{
     this.getMovies();
   }
   render(){
-    const {isLoading,movies} = this.state
-    return<div>{isLoading ? "Loading..." : movies.map(movie=>(
+    const {isLoading,movies} = this.state;
+    return ( 
+    <section class = "container">
+      {isLoading ? (
+        <div class = "loader">
+          <span class = "loader__text">Loading...</span>
+        </div>
+    ) : (
+      <div class = "movies">
+        {movies.map(movie=>(
       <Movie key ={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
-    ))}</div>;
-  };
+    ))}
+    </div>
+        )}
+      </section>
+      );
+    }
 }
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id,year,title,summary,poster}){
-    return <h4>{title}</h4>
-}
-
-Movie.propTypes = {
-    id : PropTypes.number.isRequired,
-    year : PropTypes.number.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
+function Movie({year,title,summary,poster}){
+    return <div class = "movies__movie">
+        <img src={poster} alt = {title} title = {title} />
+        <div class = "movie__data">>
+            <h3 class = "movie__title">{title}</h3>
+            <h5 class = "movie__year">{year}</h5>
+            <p class = "movie__summary">{summary}</p>
+        </div>
+    </div>
 };
 
 
