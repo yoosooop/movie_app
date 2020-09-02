ac52c43c405a0a7918063d2ea08d80c21be2cc12
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-09-03 02:29:45 +0900

    Adding Css

--- src/Movie.js
+++ src/Movie.js
@@ -3,31 +3,32 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({year,title,summary,poster,genres}){
-    return ( <div className = "movies__movie">
+    return ( 
+        <div className = "movie">
         <img src={poster} alt = {title} title = {title} />
         <div className = "movie__data">
             <h3 className = "movie__title">{title}</h3>
             <h5 className = "movie__year">{year}</h5>
-            <ul className = "genres">
+            <ul className="movie__genres">
                 {genres.map((genre,index) => (
                     <li key = {index} className="genres__genre">
                         {genre}
                     </li>
                 ))}
             </ul>
-            <p className = "movie__summary">{summary}</p>
+            <p className = "movie__summary">{summary.slice(0,140)}...</p>
         </div>
     </div>
     );
 };
 
-Movie.prototype = {
-    id : PropTypes.number.isRequired,
-    year : PropTypes.number.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired,
-    genres : PropTypes.arrayOf(PropTypes.string).isRequired
-}
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+  };
 
 export default Movie;
