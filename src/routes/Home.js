eea707aa0eb8aa9535566a684298a539bc7f45a9
yooheesoo <55743005+yooheesoo@users.noreply.github.com>
2020-09-03 11:31:47 +0900

    Getting Ready for the Router

---
+++ src/routes/Home.js
@@ -0,0 +1,77 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+
+class Home extends React.Component{
+  state = {
+    isLoading : true,
+    movie : []
+  };
+  getMovies = async() =>{
+    const {data: {data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    console.log(movies)
+    this.setState({movies, isLoading : false})
+  };
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const {isLoading,movies} = this.state;
+    return ( 
+    <section className = "container">
+      {isLoading ? (
+        <div className = "loader">
+          <span className = "loader__text">Loading...</span>
+        </div>
+    ) : (
+      <div className = "movies">
+        {movies.map(movie=>(
+      <Movie 
+        key ={movie.id} 
+        id={movie.id} 
+        year={movie.year} 
+        title={movie.title} 
+        summary={movie.summary} 
+        poster={movie.medium_cover_image} 
+        genres = {movie.genres}/>
+    ))}
+    </div>
+        )}
+      </section>
+      );
+    }
+}
+
+export default App;
+
+/*
+class App extends React.Component{
+  state = {
+    count : 0
+  }
+  add = () => {
+    console.log("add");
+    //this.setState({count : this.state.count + 1})
+    this.setState(current => ({count : current.count + 1}))
+  };
+  minus = () => {
+    console.log("minus");
+    //this.setState({count : this.state.count - 1})
+    this.setState(current => ({count : current.count - 1}))
+  };
+  render() {
+     //즉시 호출되는 것이 아니라 click 했을 때만 function이 호출되길 원한다.
+    return <div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>
+  }
+}
+
+
+export default App;
+
+*/
